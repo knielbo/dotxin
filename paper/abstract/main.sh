6716ac09f4fa8e44830ffc596784734dfd3d8d4b
knielbo <kln@cas.au.dk>
2019-05-21 19:10:26 +0200

    adds paper dir

---
+++ paper/abstract/main.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pdflatex kln_abstract_classics.tex
+rm *.aux *.log
---
+++ paper/main.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+pdflatex main.tex
+pdflatex main.tex
+rm *.aux *.log *.out *.snm *.toc *.nav
+xdg-open main.pdf
+cp main.pdf /home/knielbo/Documents/knielbo.github.io/files/kln_shangsh.pdf
